a424f72457405ae22e9f893126235d043b4f7b14
AceWong <ace-wong@outlook.com>
2016-03-28 12:07:53 -0400

    Added files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+(function() {
+
+	//"use strict";
+	console.log("JS is Working");
+
+
+	//variables
+	
+	
+	
+	
+	
+	//functions
+	
+	function () {
+		
+	}
+	
+	
+	
+	
+	//event listeners
+
+
+
+
+})();
